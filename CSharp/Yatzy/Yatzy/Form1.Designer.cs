afc98f7723618efda087923b16604cbf582a1319
unknown <gatzakbob@CMS314016.students.ccsu.edu>
2017-02-25 04:42:37 -0500

    Gotten So far

--- CSharp/Yatzy/Yatzy/Form1.Designer.cs
+++ CSharp/Yatzy/Yatzy/Form1.Designer.cs
@@ -29,46 +29,39 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.rollBox = new System.Windows.Forms.PictureBox();
-            this.rollButton = new System.Windows.Forms.Button();
             this.diceOne = new System.Windows.Forms.PictureBox();
             this.diceTwo = new System.Windows.Forms.PictureBox();
             this.diceThree = new System.Windows.Forms.PictureBox();
             this.diceFour = new System.Windows.Forms.PictureBox();
             this.diceFive = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.rollBox)).BeginInit();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.Roll = new System.Windows.Forms.Button();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox5 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.AcesButton = new System.Windows.Forms.Button();
+            this.AcesScore = new System.Windows.Forms.Label();
+            this.HoldButton0 = new System.Windows.Forms.Button();
+            this.HoldButton1 = new System.Windows.Forms.Button();
+            this.HoldButton2 = new System.Windows.Forms.Button();
+            this.HoldButton3 = new System.Windows.Forms.Button();
+            this.HoldButton4 = new System.Windows.Forms.Button();
+            this.UpperTotalScore = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.diceOne)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.diceTwo)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.diceThree)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.diceFour)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.diceFive)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // rollBox
-            // 
-            this.rollBox.Image = ((System.Drawing.Image)(resources.GetObject("rollBox.Image")));
-            this.rollBox.Location = new System.Drawing.Point(262, 74);
-            this.rollBox.Name = "rollBox";
-            this.rollBox.Size = new System.Drawing.Size(637, 150);
-            this.rollBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.rollBox.TabIndex = 0;
-            this.rollBox.TabStop = false;
-            this.rollBox.Visible = false;
-            // 
-            // rollButton
-            // 
-            this.rollButton.Location = new System.Drawing.Point(508, 230);
-            this.rollButton.Name = "rollButton";
-            this.rollButton.Size = new System.Drawing.Size(75, 23);
-            this.rollButton.TabIndex = 1;
-            this.rollButton.Text = "Roll";
-            this.rollButton.UseVisualStyleBackColor = true;
-            this.rollButton.Click += new System.EventHandler(this.rollButton_Click);
             // 
             // diceOne
             // 
             this.diceOne.Image = ((System.Drawing.Image)(resources.GetObject("diceOne.Image")));
-            this.diceOne.Location = new System.Drawing.Point(268, 92);
+            this.diceOne.Location = new System.Drawing.Point(263, 92);
             this.diceOne.Name = "diceOne";
             this.diceOne.Size = new System.Drawing.Size(109, 109);
             this.diceOne.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -115,40 +108,220 @@
             this.diceFive.TabIndex = 6;
             this.diceFive.TabStop = false;
             // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(309, 230);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(60, 55);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox2.Location = new System.Drawing.Point(426, 230);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(55, 55);
+            this.richTextBox2.TabIndex = 8;
+            this.richTextBox2.Text = "";
+            // 
+            // Roll
+            // 
+            this.Roll.Location = new System.Drawing.Point(507, 274);
+            this.Roll.Name = "Roll";
+            this.Roll.Size = new System.Drawing.Size(75, 23);
+            this.Roll.TabIndex = 9;
+            this.Roll.Text = "Roll";
+            this.Roll.UseVisualStyleBackColor = true;
+            this.Roll.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox3.Location = new System.Drawing.Point(534, 230);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(63, 55);
+            this.richTextBox3.TabIndex = 10;
+            this.richTextBox3.Text = "";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox4.Location = new System.Drawing.Point(649, 230);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(65, 55);
+            this.richTextBox4.TabIndex = 11;
+            this.richTextBox4.Text = "";
+            // 
+            // richTextBox5
+            // 
+            this.richTextBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox5.Location = new System.Drawing.Point(761, 230);
+            this.richTextBox5.Name = "richTextBox5";
+            this.richTextBox5.Size = new System.Drawing.Size(63, 55);
+            this.richTextBox5.TabIndex = 12;
+            this.richTextBox5.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(262, 64);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(616, 160);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // AcesButton
+            // 
+            this.AcesButton.Location = new System.Drawing.Point(56, 310);
+            this.AcesButton.Name = "AcesButton";
+            this.AcesButton.Size = new System.Drawing.Size(75, 23);
+            this.AcesButton.TabIndex = 14;
+            this.AcesButton.Text = "Aces";
+            this.AcesButton.UseVisualStyleBackColor = true;
+            this.AcesButton.Click += new System.EventHandler(this.AcesButton_Click);
+            // 
+            // AcesScore
+            // 
+            this.AcesScore.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.AcesScore.Location = new System.Drawing.Point(137, 314);
+            this.AcesScore.Name = "AcesScore";
+            this.AcesScore.Size = new System.Drawing.Size(137, 18);
+            this.AcesScore.TabIndex = 15;
+            // 
+            // HoldButton0
+            // 
+            this.HoldButton0.Location = new System.Drawing.Point(278, 35);
+            this.HoldButton0.Name = "HoldButton0";
+            this.HoldButton0.Size = new System.Drawing.Size(75, 23);
+            this.HoldButton0.TabIndex = 16;
+            this.HoldButton0.Text = "Hold";
+            this.HoldButton0.UseVisualStyleBackColor = true;
+            // 
+            // HoldButton1
+            // 
+            this.HoldButton1.Location = new System.Drawing.Point(387, 35);
+            this.HoldButton1.Name = "HoldButton1";
+            this.HoldButton1.Size = new System.Drawing.Size(75, 23);
+            this.HoldButton1.TabIndex = 17;
+            this.HoldButton1.Text = "Hold";
+            this.HoldButton1.UseVisualStyleBackColor = true;
+            // 
+            // HoldButton2
+            // 
+            this.HoldButton2.Location = new System.Drawing.Point(503, 35);
+            this.HoldButton2.Name = "HoldButton2";
+            this.HoldButton2.Size = new System.Drawing.Size(75, 23);
+            this.HoldButton2.TabIndex = 18;
+            this.HoldButton2.Text = "Hold";
+            this.HoldButton2.UseVisualStyleBackColor = true;
+            // 
+            // HoldButton3
+            // 
+            this.HoldButton3.Location = new System.Drawing.Point(612, 35);
+            this.HoldButton3.Name = "HoldButton3";
+            this.HoldButton3.Size = new System.Drawing.Size(75, 23);
+            this.HoldButton3.TabIndex = 19;
+            this.HoldButton3.Text = "Hold";
+            this.HoldButton3.UseVisualStyleBackColor = true;
+            // 
+            // HoldButton4
+            // 
+            this.HoldButton4.Location = new System.Drawing.Point(727, 35);
+            this.HoldButton4.Name = "HoldButton4";
+            this.HoldButton4.Size = new System.Drawing.Size(75, 23);
+            this.HoldButton4.TabIndex = 20;
+            this.HoldButton4.Text = "Hold";
+            this.HoldButton4.UseVisualStyleBackColor = true;
+            // 
+            // UpperTotalScore
+            // 
+            this.UpperTotalScore.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.UpperTotalScore.Location = new System.Drawing.Point(174, 605);
+            this.UpperTotalScore.Name = "UpperTotalScore";
+            this.UpperTotalScore.Size = new System.Drawing.Size(100, 23);
+            this.UpperTotalScore.TabIndex = 21;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 615);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Fixed Total Score:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.HighlightText;
             this.ClientSize = new System.Drawing.Size(1129, 798);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.UpperTotalScore);
+            this.Controls.Add(this.HoldButton4);
+            this.Controls.Add(this.HoldButton3);
+            this.Controls.Add(this.HoldButton2);
+            this.Controls.Add(this.HoldButton1);
+            this.Controls.Add(this.HoldButton0);
+            this.Controls.Add(this.AcesScore);
+            this.Controls.Add(this.AcesButton);
+            this.Controls.Add(this.Roll);
+            this.Controls.Add(this.richTextBox5);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.diceFive);
             this.Controls.Add(this.diceFour);
             this.Controls.Add(this.diceThree);
             this.Controls.Add(this.diceTwo);
             this.Controls.Add(this.diceOne);
-            this.Controls.Add(this.rollButton);
-            this.Controls.Add(this.rollBox);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.rollBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.diceOne)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.diceTwo)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.diceThree)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.diceFour)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.diceFive)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox rollBox;
-        private System.Windows.Forms.Button rollButton;
         private System.Windows.Forms.PictureBox diceOne;
         private System.Windows.Forms.PictureBox diceTwo;
         private System.Windows.Forms.PictureBox diceThree;
         private System.Windows.Forms.PictureBox diceFour;
         private System.Windows.Forms.PictureBox diceFive;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button Roll;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.RichTextBox richTextBox5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button AcesButton;
+        private System.Windows.Forms.Label AcesScore;
+        private System.Windows.Forms.Button HoldButton0;
+        private System.Windows.Forms.Button HoldButton1;
+        private System.Windows.Forms.Button HoldButton2;
+        private System.Windows.Forms.Button HoldButton3;
+        private System.Windows.Forms.Button HoldButton4;
+        private System.Windows.Forms.Label UpperTotalScore;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- CSharp/Yatzy/Yatzy/Form1.cs
+++ CSharp/Yatzy/Yatzy/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Threading;
 
 namespace WindowsFormsApplication1
 {
@@ -17,23 +17,195 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
         }
+        private int[] Dices = new int[5];
+        private int RollCount = 0;
+        private int ScoreCount = 0;
+        private Random random = new Random(Guid.NewGuid().GetHashCode());
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Roll.Text == "Roll")
+            {
+                pictureBox1.Visible = true;
+                diceOne.Visible = false;
+                diceTwo.Visible = false;
+                diceThree.Visible = false;
+                diceFour.Visible = false;
+                diceFive.Visible = false;
+                Roll.Text = "Stop";
+            }
+            else
+            {
+                pictureBox1.Visible = false;
+                diceOne.Visible = true;
+                diceTwo.Visible = true;
+                diceThree.Visible = true;
+                diceFour.Visible = true;
+                diceFive.Visible = true;
+                richTextBox1.Text = random.Next(1, 7).ToString();
+                Dices[0] = Int32.Parse(richTextBox1.Text);
+                richTextBox2.Text = random.Next(1, 7).ToString();
+                Dices[1] = Int32.Parse(richTextBox2.Text);
+                richTextBox3.Text = random.Next(1, 7).ToString();
+                Dices[2] = Int32.Parse(richTextBox3.Text);
+                richTextBox4.Text = random.Next(1, 7).ToString();
+                Dices[3] = Int32.Parse(richTextBox4.Text);
+                richTextBox5.Text = random.Next(1, 7).ToString();
+                Dices[4] = Int32.Parse(richTextBox5.Text);
+                Roll.Text = "Roll";
+            }
+            if (richTextBox1.Text == "1")
+            {
+                diceOne.Image = Image.FromFile(@"C:\Users\gatzakbob\Desktop\Hackathon\hackathon-competitive-ranch\CSharp\Yatzy\Yatzy\dice1.png");
+            }
+            else if (richTextBox1.Text == "2")
+            {
+                diceOne.Image = (System.Drawing.Image)Image.FromFile(@"C:\Users\gatzakbob\Desktop\Hackathon\hackathon-competitive-ranch\CSharp\Yatzy\Yatzy\dice2.png");
+            }
+            else if (richTextBox1.Text == "3")
+            {
+                diceOne.Image = (System.Drawing.Image)Image.FromFile(@"C:\Users\gatzakbob\Desktop\Hackathon\hackathon-competitive-ranch\CSharp\Yatzy\Yatzy\dice3.png");
+            }
+            else if (richTextBox1.Text == "4")
+            {
+                diceOne.Image = (System.Drawing.Image)Image.FromFile(@"C:\Users\gatzakbob\Desktop\Hackathon\hackathon-competitive-ranch\CSharp\Yatzy\Yatzy\dice4.png");
+            }
+            else if (richTextBox1.Text == "5")
+            {
+                diceOne.Image = (System.Drawing.Image)Image.FromFile(@"C:\Users\gatzakbob\Desktop\Hackathon\hackathon-competitive-ranch\CSharp\Yatzy\Yatzy\dice5.png");
+            }
+            else if (richTextBox1.Text == "6")
+            {
+                diceOne.Image = (System.Drawing.Image)Image.FromFile(@"C:\Users\gatzakbob\Desktop\Hackathon\hackathon-competitive-ranch\CSharp\Yatzy\Yatzy\dice6.png");
+            }
 
-        private void rollButton_Click(object sender, EventArgs e)
-        {             
-            rollBox.Visible = true;
-            diceOne.Visible = false;
-            diceTwo.Visible = false;
-            diceThree.Visible = false;
-            diceFour.Visible = false;
-            diceFive.Visible = false;
-            Task.Delay(2000);
-            rollBox.Visible = false;
-            diceOne.Visible = true;
-            diceTwo.Visible = true;
-            diceThree.Visible = true;
-            diceFour.Visible = true;
-            diceFive.Visible = true;
 
+            if (richTextBox2.Text == "1")
+            {
+                diceTwo.Image = Image.FromFile(@"C:\Users\gatzakbob\Desktop\Hackathon\hackathon-competitive-ranch\CSharp\Yatzy\Yatzy\dice1.png");
+            }
+            else if (richTextBox2.Text == "2")
+            {
+                diceTwo.Image = (System.Drawing.Image)Image.FromFile(@"C:\Users\gatzakbob\Desktop\Hackathon\hackathon-competitive-ranch\CSharp\Yatzy\Yatzy\dice2.png");
+            }
+            else if (richTextBox2.Text == "3")
+            {
+                diceTwo.Image = (System.Drawing.Image)Image.FromFile(@"C:\Users\gatzakbob\Desktop\Hackathon\hackathon-competitive-ranch\CSharp\Yatzy\Yatzy\dice3.png");
+            }
+            else if (richTextBox2.Text == "4")
+            {
+                diceTwo.Image = (System.Drawing.Image)Image.FromFile(@"C:\Users\gatzakbob\Desktop\Hackathon\hackathon-competitive-ranch\CSharp\Yatzy\Yatzy\dice4.png");
+            }
+            else if (richTextBox2.Text == "5")
+            {
+                diceTwo.Image = (System.Drawing.Image)Image.FromFile(@"C:\Users\gatzakbob\Desktop\Hackathon\hackathon-competitive-ranch\CSharp\Yatzy\Yatzy\dice5.png");
+            }
+            else if (richTextBox2.Text == "6")
+            {
+                diceTwo.Image = (System.Drawing.Image)Image.FromFile(@"C:\Users\gatzakbob\Desktop\Hackathon\hackathon-competitive-ranch\CSharp\Yatzy\Yatzy\dice6.png");
+            }
+
+
+            if (richTextBox3.Text == "1")
+            {
+                diceThree.Image = Image.FromFile(@"C:\Users\gatzakbob\Desktop\Hackathon\hackathon-competitive-ranch\CSharp\Yatzy\Yatzy\dice1.png");
+            }
+            else if (richTextBox3.Text == "2")
+            {
+                diceThree.Image = (System.Drawing.Image)Image.FromFile(@"C:\Users\gatzakbob\Desktop\Hackathon\hackathon-competitive-ranch\CSharp\Yatzy\Yatzy\dice2.png");
+            }
+            else if (richTextBox3.Text == "3")
+            {
+                diceThree.Image = (System.Drawing.Image)Image.FromFile(@"C:\Users\gatzakbob\Desktop\Hackathon\hackathon-competitive-ranch\CSharp\Yatzy\Yatzy\dice3.png");
+            }
+            else if (richTextBox3.Text == "4")
+            {
+                diceThree.Image = (System.Drawing.Image)Image.FromFile(@"C:\Users\gatzakbob\Desktop\Hackathon\hackathon-competitive-ranch\CSharp\Yatzy\Yatzy\dice4.png");
+            }
+            else if (richTextBox3.Text == "5")
+            {
+                diceThree.Image = (System.Drawing.Image)Image.FromFile(@"C:\Users\gatzakbob\Desktop\Hackathon\hackathon-competitive-ranch\CSharp\Yatzy\Yatzy\dice5.png");
+            }
+            else if (richTextBox3.Text == "6")
+            {
+                diceThree.Image = (System.Drawing.Image)Image.FromFile(@"C:\Users\gatzakbob\Desktop\Hackathon\hackathon-competitive-ranch\CSharp\Yatzy\Yatzy\dice6.png");
+            }
+
+
+            if (richTextBox4.Text == "1")
+            {
+                diceFour.Image = Image.FromFile(@"C:\Users\gatzakbob\Desktop\Hackathon\hackathon-competitive-ranch\CSharp\Yatzy\Yatzy\dice1.png");
+            }
+            else if (richTextBox4.Text == "2")
+            {
+                diceFour.Image = (System.Drawing.Image)Image.FromFile(@"C:\Users\gatzakbob\Desktop\Hackathon\hackathon-competitive-ranch\CSharp\Yatzy\Yatzy\dice2.png");
+            }
+            else if (richTextBox4.Text == "3")
+            {
+                diceFour.Image = (System.Drawing.Image)Image.FromFile(@"C:\Users\gatzakbob\Desktop\Hackathon\hackathon-competitive-ranch\CSharp\Yatzy\Yatzy\dice3.png");
+            }
+            else if (richTextBox4.Text == "4")
+            {
+                diceFour.Image = (System.Drawing.Image)Image.FromFile(@"C:\Users\gatzakbob\Desktop\Hackathon\hackathon-competitive-ranch\CSharp\Yatzy\Yatzy\dice4.png");
+            }
+            else if (richTextBox4.Text == "5")
+            {
+                diceFour.Image = (System.Drawing.Image)Image.FromFile(@"C:\Users\gatzakbob\Desktop\Hackathon\hackathon-competitive-ranch\CSharp\Yatzy\Yatzy\dice5.png");
+            }
+            else if (richTextBox4.Text == "6")
+            {
+                diceFour.Image = (System.Drawing.Image)Image.FromFile(@"C:\Users\gatzakbob\Desktop\Hackathon\hackathon-competitive-ranch\CSharp\Yatzy\Yatzy\dice6.png");
+            }
+
+            if (richTextBox5.Text == "1")
+            {
+                diceFive.Image = Image.FromFile(@"C:\Users\gatzakbob\Desktop\Hackathon\hackathon-competitive-ranch\CSharp\Yatzy\Yatzy\dice1.png");
+            }
+            else if (richTextBox5.Text == "2")
+            {
+                diceFive.Image = (System.Drawing.Image)Image.FromFile(@"C:\Users\gatzakbob\Desktop\Hackathon\hackathon-competitive-ranch\CSharp\Yatzy\Yatzy\dice2.png");
+            }
+            else if (richTextBox5.Text == "3")
+            {
+                diceFive.Image = (System.Drawing.Image)Image.FromFile(@"C:\Users\gatzakbob\Desktop\Hackathon\hackathon-competitive-ranch\CSharp\Yatzy\Yatzy\dice3.png");
+            }
+            else if (richTextBox5.Text == "4")
+            {
+                diceFive.Image = (System.Drawing.Image)Image.FromFile(@"C:\Users\gatzakbob\Desktop\Hackathon\hackathon-competitive-ranch\CSharp\Yatzy\Yatzy\dice4.png");
+            }
+            else if (richTextBox5.Text == "5")
+            {
+                diceFive.Image = (System.Drawing.Image)Image.FromFile(@"C:\Users\gatzakbob\Desktop\Hackathon\hackathon-competitive-ranch\CSharp\Yatzy\Yatzy\dice5.png");
+            }
+            else if (richTextBox5.Text == "6")
+            {
+                diceFive.Image = (System.Drawing.Image)Image.FromFile(@"C:\Users\gatzakbob\Desktop\Hackathon\hackathon-competitive-ranch\CSharp\Yatzy\Yatzy\dice6.png");
+            }
         }
+
+        private void AcesButton_Click(object sender, EventArgs e)
+        {
+            if ((RollCount > 0) && (AcesScore.Text == ""))
+            {
+                int Score;// = TheScore.AddUpDice(1, Dices);
+
+                string message = "The Total is " + Score.ToString() + ". Do you wish to accept this?";
+                DialogResult result;
+
+                // Displays the MessageBox.
+                result = MessageBox.Show(message,
+                                  "Aces Total",
+                                  MessageBoxButtons.YesNo,
+                                  MessageBoxIcon.Question);
+
+                if (result == DialogResult.Yes)
+                {
+                    AcesScore.Text = Score.ToString();
+                    //TheScore.UpdateTotals(Score, true);
+                    //Reset();
+                }
+            }
+        }
+       
     }
+
 }
