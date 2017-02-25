24a19f7cd0b7fec44b15f4fe01754f83f332a478
Nilay <nilaybhatt@my.ccsu.edu>
2017-02-25 07:17:00 -0500

    BigTravel

---
+++ src/edu/Ranch/BigTravel/Main.java
@@ -0,0 +1,87 @@
+package edu.Ranch.BigTravel;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.abs;
+
+/**
+ * Created by Nilay on 2/25/2017.
+ */
+public class Main {
+
+    private static ArrayList<Point> coordinates = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(args[0]));
+        String inputText;
+        int numberOfCities;
+        boolean flag = true;
+        while ((inputText = br.readLine()) != null) {
+            if (flag) {
+                numberOfCities = Integer.parseInt(inputText);
+                flag = false;
+            } else {
+                StringTokenizer st = new StringTokenizer(inputText);
+                Point p = new Point(Double.parseDouble(st.nextElement().toString()), Double.parseDouble(st.nextElement().toString()));
+                coordinates.add(p);
+            }
+        }
+//        coordinates.add(new Point(-1.0,5.0));
+//        coordinates.add(new Point(1.0,6.0));
+//        coordinates.add(new Point(3.0,5.0));
+//        coordinates.add(new Point(2.0,3.0));
+
+        double totalDistance = calculateAllDistance(coordinates);
+
+        double finalDistance = totalDistance % (Math.pow(10.0, 9.0) + 7);
+        //System.out.print(finalDistance);
+        try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream(args[1])))) {
+            writer.write(Integer.parseInt(finalDistance + "") + "");
+
+        }
+    }
+
+    private static double calculateDistance(Point a, Point b){
+        return abs(a.getX() - b.getX()) + abs(a.getY() -b.getY());
+    }
+
+    private static double calculateAllDistance(ArrayList<Point>points){
+        double totalDistance = 0;
+        for(int i = 0; i < points.size(); i++){
+            for(int j = i; j < points.size(); j++){
+                totalDistance+= calculateDistance(points.get(i),points.get(j));
+            }
+        }
+
+        return totalDistance;
+    }
+
+    class Point {
+        private double x,y;
+
+        public Point(double x, double y){
+            this.x = x;
+            this.y = y;
+        }
+        public double getX() {
+            return x;
+        }
+
+        public void setX(double x) {
+            this.x = x;
+        }
+
+        public double getY() {
+            return y;
+        }
+
+        public void setY(double y) {
+            this.y = y;
+        }
+    }
+
+
+}
